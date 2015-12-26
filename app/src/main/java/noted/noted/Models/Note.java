2ed69de63566b7b7631a4069a7fb0a2444dded94
adisimantov <sadis66@gmail.com>
2015-12-26 19:39:22 +0200

    add tabs received notes and sent noted

---
+++ app/src/main/java/noted/noted/Models/Note.java
@@ -0,0 +1,71 @@
+package noted.noted.Models;
+
+import android.location.Location;
+
+import java.util.Calendar;
+
+/**
+ * Created by adi on 26-Dec-15.
+ */
+public class Note {
+        int id;
+        int from;
+        int to;
+        String details;
+        Calendar time;
+        Location location;
+        boolean isAppear;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public void setFrom(int from) {
+        this.from = from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public void setTo(int to) {
+        this.to = to;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Calendar getTime() {
+        return time;
+    }
+
+    public void setTime(Calendar time) {
+        this.time = time;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public boolean isAppear() {
+        return isAppear;
+    }
+
+    public void setIsAppear(boolean isAppear) {
+        this.isAppear = isAppear;
+    }
+}
+
