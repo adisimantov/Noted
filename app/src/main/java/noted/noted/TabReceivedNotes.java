991a3c192427f96a62c19a00779fe996c1b0c2f7
adisimantov <sadis66@gmail.com>
2015-12-29 19:49:41 +0200

    set notes grid

--- app/src/main/java/noted/noted/TabReceivedNotes.java
+++ app/src/main/java/noted/noted/TabReceivedNotes.java
@@ -1,12 +1,15 @@
 package noted.noted;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.GridView;
 import android.widget.TextView;
 
@@ -18,7 +21,7 @@ import noted.noted.Models.Note;
 /**
  * Created by adi on 26-Dec-15.
  */
-public class TabReceivedNotes extends Fragment{
+public class TabReceivedNotes extends Fragment {
 
     GridView receivedGrid;
 
@@ -26,17 +29,22 @@ public class TabReceivedNotes extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.received_notes_tab, container, false);
         receivedGrid = (GridView) view.findViewById(R.id.receivedNotesGrid);
-        receivedGrid.setAdapter(new MyAdapter(view.getContext()));
-        return inflater.inflate(R.layout.received_notes_tab, container, false);
+        receivedGrid.setAdapter(new GridAdapter(view.getContext()));
+        return view;
     }
 
-    private class MyAdapter extends BaseAdapter {
+    private class GridAdapter extends BaseAdapter {
 
         List<Note> lNotes = new ArrayList<Note>();
+        Context context;
 
-        public MyAdapter(Context context){
+        public GridAdapter(Context context) {
+            this.context = context;
+            lNotes.add(new Note(1, 1111, 2222, "blabla", false));
+            lNotes.add(new Note(2, 1111, 2222, "blabla", false));
+            lNotes.add(new Note(3, 1111, 2222, "blabla", false));
+            lNotes.add(new Note(4, 1111, 2222, "blabla", false));
 
-            lNotes.add(new Note(1,1111,2222,"blabla",false));
         }
 
         @Override
@@ -56,21 +64,21 @@ public class TabReceivedNotes extends Fragment{
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            View v = convertView;
-            TextView from;
-            TextView to;
-            TextView details;
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.grid_note_item, null);
+            }
 
-            from = (TextView) v.getTag(R.id.noteFrom);
-            to = (TextView) v.getTag(R.id.noteTo);
-            details = (TextView) v.getTag(R.id.noteDetails);
+            TextView from = (TextView) convertView.findViewById(R.id.noteFrom);
+            TextView to = (TextView) convertView.findViewById(R.id.noteTo);
+            TextView details = (TextView) convertView.findViewById(R.id.noteDetails);
 
             Note note = getItem(position);
-            from.setText(note.getFrom());
-            to.setText(note.getTo());
-            details.setText(note.getDetails());
+            from.setText(note.getFrom()+"");
+            to.setText(note.getTo()+"");
+            details.setText(note.getDetails()+"");
 
-            return v;
+            return convertView;
         }
     }
 }
