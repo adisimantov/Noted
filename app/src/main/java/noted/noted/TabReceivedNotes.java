64686890cda7ff44a777b8fd7de6187eb85b494c
noylevy <levynoy12@gmail.com>
2016-01-12 21:44:03 +0200

    Delete note

--- app/src/main/java/noted/noted/TabReceivedNotes.java
+++ app/src/main/java/noted/noted/TabReceivedNotes.java
@@ -1,12 +1,15 @@
 package noted.noted;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.GridView;
@@ -29,6 +32,32 @@ public class TabReceivedNotes extends Fragment {
         View view = inflater.inflate(R.layout.received_notes_tab, container, false);
         receivedGrid = (GridView) view.findViewById(R.id.receivedNotesGrid);
         receivedGrid.setAdapter(new GridAdapter(view.getContext(), true));
+
+        receivedGrid.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, final View view, final int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+                builder.setMessage(R.string.delete_note_message);
+                builder.setPositiveButton(R.string.yes_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                builder.setNegativeButton(R.string.no_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+
+                return true;
+            }
+        });
+
         return view;
     }
 }
