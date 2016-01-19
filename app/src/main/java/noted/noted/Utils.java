8340c80dc5a304642e2a7379748883d851dcc6e9
Anna <ankol24@gmail.com>
2016-01-20 01:50:29 +0200

    code fixes

--- app/src/main/java/noted/noted/Utils.java
+++ app/src/main/java/noted/noted/Utils.java
@@ -9,4 +9,6 @@ public class Utils {
     public static final String NOTE_FROM_PARAM = "NOTE_FROM";
     public static final String NOTE_TO_PARAM = "NOTE_TO";
     public static final String NOTE_DETAILS_PARAM = "NOTE_DETAILS";
+
+    public static final String LTR_CHAR = "\u200E";
 }
